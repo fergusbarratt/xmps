7969db4cd6614f6c03c0b110c217b5b5769e5b8c
Fergus Barratt <fergus.barratt00@gmail.com>
2018-09-26 09:47:19 +0100

    otoc runs

--- scripts/otoc_lyapunov_comparison.py
+++ scripts/otoc_lyapunov_comparison.py
@@ -8,35 +8,35 @@ from fTDVP import Trajectory
 from spin import N_body_spins, spins, n_body
 from numpy import load, linspace, save, sum, log, array
 import matplotlib.pyplot as plt
-Sx1, Sy1, Sz1 = N_body_spins(0.5, 1, 7)
-Sx2, Sy2, Sz2 = N_body_spins(0.5, 2, 7)
-Sx3, Sy3, Sz3 = N_body_spins(0.5, 3, 7)
-Sx4, Sy4, Sz4 = N_body_spins(0.5, 4, 7)
-Sx5, Sy5, Sz5 = N_body_spins(0.5, 5, 7)
-Sx6, Sy6, Sz6 = N_body_spins(0.5, 6, 7)
-Sx7, Sy7, Sz7 = N_body_spins(0.5, 7, 7)
+L = 8
+S_list = [N_body_spins(0.5, n, L) for n in range(1, L+1)]
+n = 2
+m = 4
+Sx1, Sy1, Sz1 = S_list[n]
+SxL, SyL, SzL = S_list[m]
 
 Sx12, Sy12, Sz12 = N_body_spins(0.5, 1, 2)
 Sx22, Sy22, Sz22 = N_body_spins(0.5, 2, 2)
-listH = [Sz12@Sz22+Sx12+Sz12] + [Sz12@Sz22+Sx12+Sz12+Sx22+Sz22 for _ in range(4)] + [Sz12@Sz22+Sx22+Sz22]
+listH = [Sz12@Sz22+Sx12+Sz12] + [Sz12@Sz22+Sx12+Sz12+Sx22+Sz22 for _ in range(L-3)] + [Sz12@Sz22+Sx22+Sz22]
 fullH = sum([n_body(a, i, len(listH), d=2) for i, a in enumerate(listH)], axis=0)
 
 def numdiff(F, T):
     return array([F[n+1]-F[n]/(T[n+1]-T[n]) for n in range(len(F)-1)])
 
-mat = load('fixtures/mat7x7.npy')
+mat = load('fixtures/mat{}x{}.npy'.format(L, L))
 mps = fMPS().left_from_state(mat)
 
-T = linspace(0, 30, 200)
+T = linspace(0, 50, 200)
 
-ops = Sz1, Sz7
-otocs = Trajectory(mps, fullH, fullH=True).OTOC(T, ops)
+ops = Sz1, SzL
+otocs = Trajectory(mps, fullH, fullH=True).ed_OTOC(T, ops)
 
 fig, ax = plt.subplots(3, 1, sharex=True)
-ax[0].set_title('$Sz^1, Sz^7, L=7$', loc='right')
+ax[0].set_title('$Sz^{}, Sz^{}, L={}$'.format(n+1, m+1, L), loc='right')
 ax[0].plot(T, otocs)
-ax[1].plot(T, log(otocs))
-ax[2].plot(T[:-1], numdiff(log(array(otocs)), T))
+ax[1].plot(T, otocs)
+ax[2].plot(T[:-1], numdiff(log(otocs), T))
+ax[1].set_yscale('log')
 fig.tight_layout()
-fig.savefig('images/Sz1Sz7.pdf', bbox_inches='tight')
+#fig.savefig('images/Sz1Sz7.pdf', bbox_inches='tight')
 plt.show()
