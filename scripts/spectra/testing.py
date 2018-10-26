adb5c25f333d5ee9dca636ad650998406ea959f1
Fergus Barratt <k1508759@kcl.ac.uk>
2018-10-26 12:15:54 +0100

    stuff

--- scripts/spectra/testing.py
+++ scripts/spectra/testing.py
@@ -1,6 +1,6 @@
 import os, sys, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0,parentdir)
 
 from fMPS import fMPS
@@ -16,13 +16,13 @@ Sx22, Sy22, Sz22 = N_body_spins(0.5, 2, 2)
 
 Sx, Sy, Sz = spins(0.5)
 
-L = 6 
+L = 6
 bulkH =Sz12@Sz22+Sx22
-H = [Sz12@Sz22+Sx12+Sx22] + [bulkH for _ in range(L-2)] 
+H = [Sz12@Sz22+Sx12+Sx22] + [bulkH for _ in range(L-2)]
 W = L*[MPO_TFI(0, 0.25, 0.5, 0.)]
 
-dt = 1e-2
-t_fin = 200 
+dt = 5e-3
+t_fin = 800
 T = linspace(0, t_fin, int(t_fin//dt)+1)
 
 if L<10:
@@ -31,13 +31,13 @@ if L<10:
 else:
     mps = fMPS().load('fixtures/product{}.npy'.format(L))
 
-Ds = [1, 2, 3, 4, 5, 6, 7, 8] 
+Ds = [4, 8]
 for D in Ds:
     #fig, ax = plt.subplots(4, 1)
     F = Trajectory(mps, H=H, W=W, continuous=True)
     F.run_name = 'spectra/lyapunovs'
-    exps, lys = F.lyapunov(T, D, t_burn=5, initial_basis=Q)
-    F.save(exps=True)
+    exps, lys = F.lyapunov(T, D, t_burn=10)
+    F.stop()
     #ax[0].plot(F.mps_history)
     #ax[1].plot(F.vs)
     #ax[2].plot(lys)
