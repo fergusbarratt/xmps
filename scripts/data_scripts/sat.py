ac7dbcb636f484888422f21c0ef560e7912efc01
Fergus Barratt <fergus.barratt00@gmail.com>
2018-10-31 16:46:58 +0000

    average over initial conditions. all in sat.py now

--- scripts/data_scripts/sat.py
+++ scripts/data_scripts/sat.py
@@ -3,26 +3,52 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from numpy import load, cumsum as cs, arange as ar, expand_dims as ed
-from numpy import array, log, save, exp, vectorize
+from numpy import array, log, save, exp, vectorize, linspace, log
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 
 import matplotlib.pyplot as plt
+
+dt = 0.1
+
+# rolling time average (divided by dt)
 def av(lys, dt=1):
     return (1/dt)*cs(array(lys), axis=0)/ar(1, len(lys)+1)
+# simple numerical integration
+def inte(lys, dt=1):
+    return dt*cs(array(lys), axis=0)
+# simple numerical differentiation
+def numdiff(F, T=None):
+    if T is None:
+        T = linspace(0, len(F), len(F)//dt+1)
+    return array([(F[n+1]-F[n])/(T[n+1]-T[n]) for n in range(len(F)-1)])
+# rolling max - make function monotonic
+def rm(lys):
+    a = [lys[0]]
+    for i in lys:
+        a.append(i if i>a[-1] else a[-1])
+    return a
+
 Y = []
-for D in range(2, 9):
+# load all the entanglement entropies - time average, and add last element to Y
+Ds = array([2, 3, 4, 5, 6, 7])
+for D in Ds:
     X = load('data/S_{}.npy'.format(D))
-    plt.plot(X, label=str(D))
+    T = linspace(0, 300, len(X))
+    plt.plot(T, X, label=str(D))
     Y.append(av(X)[-1])
+plt.ylabel('$S_E(t)$')
+plt.xlabel('$t$')
+plt.legend()
+#plt.savefig('images/sat/entanglement.pdf')
 plt.show()
 
-
-Ds = array([2, 3, 4, 5, 6, 7])
 dat = array(Y)
 
+# g_ is a linear interpolation of D from saturation entanglement entropy
 g_ = interp1d(dat, Ds)
 
+# make a D function that takes an entropy and gives a bond dimension
 def D(s):
     if s<min(dat):
         return 1
@@ -31,12 +57,62 @@ def D(s):
     else:
         return g_(s)
 
+D = vectorize(D)
 
+plt.scatter(D(dat), dat)
+plt.xlabel('$D$')
+plt.ylabel('Saturation $S_E$')
+plt.savefig('images/sat/sat_SE_vs_D.pdf')
+plt.show()
+
+# Functions for maximum lyapunov exponent and kolmogorov sinai entropy
 Ds_ = array([1, 2, 3, 4, 5, 6, 7, 8])
 lamb = load('data/exps.npy')
 max_l = array(list(map(max, map(abs, lamb))))
 ks = array(list(map(sum, map(abs, lamb))))/2
 
-D = vectorize(D)
+# lambda(D) and KS(D) - data is v. poor for these atm 
 max_l = interp1d(Ds_, max_l)
 ks = interp1d(Ds_, ks)
+
+plt.plot(Ds_, max_l(Ds_), label='$\lambda_{max}(D)$')
+plt.plot(Ds_, ks(Ds_), label='$S_{KS}(D)$')
+plt.legend()
+plt.xlabel('$D$')
+plt.savefig('images/sat/lambda_max_ks.pdf')
+plt.show()
+
+fig, ax = plt.subplots(2, 2, sharex=True)
+C = load('data/otocs.npy')
+S = load('data/S.npy')
+factor = (D(S))**2
+ax[0][0].plot(numdiff(C))
+ax[0][0].set_title('$\partial_t C(t)$')
+ax[1][0].plot(max_l(rm(D(S)))[:-1])
+ax[1][0].set_title('$\lambda_{max}(D(t))$')
+ax[1][1].plot(ks(rm(D(S)))[:-1]/factor)
+ax[1][1].set_title('$S_{KS}(D(t))/D(t)^2$')
+ax[0][1].plot(numdiff(S))
+ax[0][1].set_title('$\partial_t S_E(t)$')
+ax[0][1].set_xlabel('$t$')
+ax[1][1].set_xlabel('$t$')
+plt.tight_layout()
+plt.savefig('images/sat/diff_otoc_lambda_S_ks.pdf')
+plt.show()
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(numdiff(S), label ='$\partial_t S_E(t)$')
+ax.plot(ks(rm(D(S)))[:-1]/factor, label='$S_{KS}(D(t))/(D(t)^2)$')
+ax.set_xlabel('$t$')
+plt.savefig('images/sat/dS_Ks.pdf')
+plt.legend()
+plt.show()
+
+
+fig, ax = plt.subplots(1, 1)
+factor = S[:-1]
+ax.plot(numdiff(C)[1:]/C[1:-1], label='$\partial_t ln(C(t)')
+ax.plot(2*max_l(rm(D(S)))[1:-1]/factor, label='$2*\lambda_{max}(D(t)/S_E(t)$')
+plt.savefig('images/sat/dlnC_lmax.pdf')
+ax.set_ylim([-2, 5])
+plt.show()
--- scripts/ed/entanglement.py
+++ scripts/ed/entanglement.py
@@ -8,6 +8,7 @@ from fMPS import fMPS
 from spin import N_body_spins, spins, n_body
 from numpy import load, linspace, save, sum, array, concatenate as ct, stack as st, log, exp
 from numpy import ceil, real as re, cumsum as cs, arange as ar, array, sqrt
+from numpy import mean
 from numpy import log2
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -15,7 +16,7 @@ from tdvp.tdvp_fast import MPO_TFI
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
 mpl.style.use('ggplot')
-L = 6
+L=6
 S_list = [N_body_spins(0.5, n, L) for n in range(1, L+1)]
 i = 2
 j = 2
@@ -25,7 +26,7 @@ Sxj, Syj, Szj = S_list[j]
 Sx1, Sy1, Sz1 = N_body_spins(0.5, 1, 2)
 Sx2, Sy2, Sz2 = N_body_spins(0.5, 2, 2)
 
-g, h = (sqrt(5)+2)/8, (sqrt(5)+1)/4
+#g, h = (sqrt(5)+2)/8, (sqrt(5)+1)/4
 #ent = 4*Sz1@Sz2
 #loc = 2*g*Sx1+2*h*Sz1, 2*g*Sx2+2*h*Sz2
 ent = Sz1@Sz2
@@ -36,28 +37,40 @@ listH = [ent+loc[0]+loc[1]] + [ent+loc[1] for _ in range(L-2)]
 #listH[-1]-= 2*(h-1)*(Sz2)
 fullH = sum([n_body(a, i, len(listH), d=2) for i, a in enumerate(listH)], axis=0)
 
-dt = 0.1
-t_fin = 300
-D = 2**(L//2)
-T = linspace(0, t_fin, int(t_fin//dt)+1)
-T = linspace(0, 300, 2000)
-#psi_0 = load('fixtures/mat{}x{}.npy'.format(L,L))
+D = 8
+L = 6
 
-#mps = fMPS().left_from_state(psi_0).left_canonicalise(1).expand(D)
-mps = fMPS().load('fixtures/product{}.npy'.format(L)).left_canonicalise().expand(D)
 
-F = Trajectory(mps, H=fullH, fullH=True)
-F.run_name = 'spectra/entanglement'
-F.edint(T)
-sch = array(F.schmidts())
-λ = array([max([-re(s@log(s)) for s in S]) for S in sch])
-save('data/S', λ)
+# generate a list of product states with the same energy
+mpss = Trajectory(fMPS().load('fixtures/product{}.npy'.format(L)),
+                  H=listH,
+                  W=L*[MPO_TFI(0, 0.25, 0.5, 0.5)]).invfreeint(
+                          linspace(0, 300, 300), 'high').mps_list()
+e = []
+for mps in mpss:
+    psi = mps.recombine().reshape(-1)
+    e.append(psi.conj()@fullH@psi)
+plt.plot(e)
+plt.show()
+
+Ls = []
+for mps in mpss:
+    T = linspace(0, 100, 1001)
+    F = Trajectory(mps, H=fullH, fullH=True)
+    F.run_name = 'spectra/entanglement'
+    F.edint(T)
+    sch = array(F.schmidts())
+    λ = array([max([-re(s@log(s)) for s in S]) for S in sch])
+    Ls.append(λ)
+#save('data/S', λ)
 
 fig, ax = plt.subplots(1, 1, sharex=True)
-ax.plot(T[1:], λ)
-ax.set_title('$S_E$', loc='right')
-#fig.savefig('images/spectra/entanglement.pdf')
+ax.plot(T[1:], array(Ls).T)
+ax.plot(T[1:],  mean(array(Ls), axis=0), c='black', label='$\\overline{S_E(t)}$')
+ax.set_ylabel('$S_E(t)$')
+ax.set_xlabel('t')
+plt.legend()
 
-fig.tight_layout()
-fig.savefig('images/spectra/Dt.pdf')
+fig.savefig('images/sat/S.pdf')
+save('data/S', mean(array(Ls), axis=0))
 plt.show()
