306ec1ae7ef0635f92a0c5b66c71a5b346915bc7
Fergus Barratt <fergus.barratt00@gmail.com>
2018-11-19 11:57:36 +0000

    otoc fitting

--- scripts/data_scripts/sat.py
+++ scripts/data_scripts/sat.py
@@ -37,13 +37,13 @@ Ds = array([2, 3, 4, 5, 6, 7])
 for D in Ds:
     X = load('data/S_{}.npy'.format(D))
     T = linspace(0, 300, len(X))
-    plt.plot(T, X, label=str(D))
+    #plt.plot(T, X, label=str(D))
     Y.append(av(X)[-1])
-plt.ylabel('$S_E(t)$')
-plt.xlabel('$t$')
-plt.legend()
-plt.savefig('images/sat/entanglement.pdf')
-plt.show()
+#plt.ylabel('$S_E(t)$')
+#plt.xlabel('$t$')
+#plt.legend()
+#plt.savefig('images/sat/entanglement.pdf')
+#plt.show()
 
 dat = array(Y)
 
@@ -53,7 +53,7 @@ g_ = interp1d(dat, Ds)
 # make a D function that takes an entropy and gives a bond dimension
 def D(s):
     if s<min(dat):
-        return 1
+        return 2
     if s>max(dat):
         return 8
     else:
@@ -61,11 +61,11 @@ def D(s):
 
 D = vectorize(D)
 
-plt.scatter(D(dat), dat)
-plt.xlabel('$D$')
-plt.ylabel('Saturation $S_E$')
-plt.savefig('images/sat/sat_SE_vs_D.pdf')
-plt.show()
+#plt.scatter(D(dat), dat)
+#plt.xlabel('$D$')
+#plt.ylabel('Saturation $S_E$')
+#plt.savefig('images/sat/sat_SE_vs_D.pdf')
+#plt.show()
 
 # Functions for maximum lyapunov exponent and kolmogorov sinai entropy
 Ds_ = array([1, 2, 3, 4, 5, 6, 7, 8])
@@ -77,46 +77,68 @@ ks = array(list(map(sum, map(abs, lamb))))/2
 max_l = interp1d(Ds_, max_l)
 ks = interp1d(Ds_, ks)
 
-fig, ax = plt.subplots(1, 1, figsize=(7, 4))
-ax.scatter(Ds_, max_l(Ds_), label='$\lambda_{max}(D)$', marker='x')
-ax.scatter(Ds_, ks(Ds_), label='$S_{KS}(D)$', marker='x')
-ax.legend()
-ax.set_xlabel('$D$')
-fig.savefig('images/sat/lambda_max_ks.pdf')
-plt.show()
+#fig, ax = plt.subplots(1, 1, figsize=(7, 4))
+#ax.scatter(Ds_, max_l(Ds_), label='$\lambda_{max}(D)$', marker='x')
+#ax.scatter(Ds_, ks(Ds_), label='$S_{KS}(D)$', marker='x')
+#ax.legend()
+#ax.set_xlabel('$D$')
+#fig.savefig('images/sat/lambda_max_ks.pdf')
+#plt.show()
 
-fig, ax = plt.subplots(2, 2, sharex=True)
+#fig, ax = plt.subplots(2, 2, sharex=True)
 C = load('data/otocs.npy')
 S = load('data/S.npy')
 factor = (rm(D(S)))**2
-ax[0][0].plot(numdiff(C))
-ax[0][0].set_title('$\partial_t C(t)$')
-ax[1][0].plot(max_l(rm(D(S))))
-ax[1][0].set_title('$\lambda_{max}(D(t))$')
-ax[1][1].plot(ks(rm(D(S)))/factor)
-ax[1][1].set_title('$S_{KS}(D(t))/D(t)^2$')
-ax[0][1].plot(numdiff(S))
-ax[0][1].set_title('$\partial_t S_E(t)$')
-ax[0][1].set_xlabel('$t$')
-ax[1][1].set_xlabel('$t$')
-plt.tight_layout()
-plt.savefig('images/sat/diffC_otoc_lambda_S_ks.pdf')
+#ax[0][0].plot(numdiff(C))
+#ax[0][0].set_title('$\partial_t C(t)$')
+#ax[1][0].plot(max_l(rm(D(S))))
+#ax[1][0].set_title('$\lambda_{max}(D(t))$')
+#ax[1][1].plot(ks(rm(D(S)))/factor)
+#ax[1][1].set_title('$S_{KS}(D(t))/D(t)^2$')
+#ax[0][1].plot(numdiff(S))
+#ax[0][1].set_title('$\partial_t S_E(t)$')
+#ax[0][1].set_xlabel('$t$')
+#ax[1][1].set_xlabel('$t$')
+#plt.tight_layout()
+#plt.savefig('images/sat/diffC_otoc_lambda_S_ks.pdf')
+#plt.show()
+
+#fig, ax = plt.subplots(1, 1)
+#ax.plot(numdiff(S), label ='$\partial_t S_E(t)$')
+#ax.plot(ks(rm(D(S)))/factor, label='$S_{KS}(D(t))/(D(t)^2)$')
+#ax.set_xlabel('$t$')
+#plt.legend()
+#plt.savefig('images/sat/dS_Ks.pdf')
+#plt.show()
+
+from numpy import flip as fl, sqrt, exp
+fig, ax = plt.subplots(1, 1)
+x = array(range(len(numdiff(log(C[1:])))))
+
+
+def f(x, a, b, c): return 1/(a*exp(b*x)-c)
+
+params, _ = curve_fit(f, x[1:], numdiff(log(C[1:]))[1:])
+
+ax.plot(x, numdiff(log(C[1:])), label='$\partial_t ln(C(t)$')
+ax.plot(f(x, *params))
 plt.show()
 
-fig, ax = plt.subplots(1, 1)
-ax.plot(numdiff(S), label ='$\partial_t S_E(t)$')
-ax.plot(ks(rm(D(S)))/factor, label='$S_{KS}(D(t))/(D(t)^2)$')
-ax.set_xlabel('$t$')
-plt.legend()
-plt.savefig('images/sat/dS_Ks.pdf')
+def g(x, a, b, c): return 1/(c*exp(b*x)+a)
+#def g(x, a, b): return a*x**b
+x_ = array(range(len(2*max_l(rm(D(S))))))
+plt.plot(x_, 2*max_l(rm(D(S))))
+params_, _ = curve_fit(g, x_, 2*max_l(rm(D(S))))
+print(params_, params)
+plt.plot(x_, g(x_, *params_))
 plt.show()
-
-fig, ax = plt.subplots(1, 1)
-factor = 1
-ax.plot(numdiff(log(C[1:])), label='$\partial_t ln(C(t)$')
-ax.plot(2*max_l(rm(D(S)))[:(len(C)+1)], label='$2\lambda_{max}(D(t))$')
-plt.legend()
+#
+#x = range(len(numdiff(log(C[1:]))))
+#ax.plot(ks(rm(D(S)))/factor, label='$S_{KS}(D(t))/(D(t)^2)$')
+#ax.plot(fl(2*av(fl(max_l(rm(D(S)))[:(len(C)+1)], 0)), 0), label='$2\lambda_{max}(D(t))$')
+#ax.plot(2*max_l(rm(D(S)))[:(len(C)+1)], label='$2\lambda_{max}(D(t))$')
+#plt.legend()
 #ax.set_ylim([-2, 5])
 #plt.savefig('images/sat/dlnC_lmax.pdf')
-plt.show()
+#plt.show()
 
