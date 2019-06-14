6ca47a9f3b774cb5d892dc50201da4b5049d0634
Fergus Barratt <fergus.barratt00@gmail.com>
2019-06-14 12:13:16 +0100

    cleanup, renaming, packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(name='tmps',
+setup(name='xmps',
       version='0.1',
-      description='mps tangent space methods',
-      url = 'https://github.com/fergusbarratt/mps',
+      description='tangent space methods for Matrix Product State',
+      url = 'https://github.com/fergusbarratt/xmps',
       author='Fergus Barratt',
       author_email='fergus.barratt@kcl.ac.uk',
       license='GPL',
